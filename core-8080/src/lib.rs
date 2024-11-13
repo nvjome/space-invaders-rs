322c5553279a4fd1f52205ff087e28ff46c2f143
nvjome <94138462+nvjome@users.noreply.github.com>
2024-11-13 16:39:12 -0600

    start execute function

--- core-8080/src/lib.rs
+++ core-8080/src/lib.rs
@@ -4,14 +4,14 @@ const NUM_REGISTERS: usize = 6;
 pub struct CPU {
     general_register: [u8; NUM_REGISTERS],
     memory: [u8; MEM_SIZE],
-    accumulator: u8,
     program_counter: u16,
     stack: Vec<u16>,
-    b_reg: u8,
+    a_reg: u8, // accumulator
+    b_reg: u8, // msb when paired with c
     c_reg: u8,
-    d_reg: u8,
+    d_reg: u8, // msb when paired with e
     e_reg: u8,
-    h_reg: u8,
+    h_reg: u8, // msb when paired with h
     l_reg: u8,
     condition_flags: ConditionFlags,
 }
@@ -25,7 +25,26 @@ struct ConditionFlags {
 }
 
 impl CPU {
-    pub fn cycle() {
-        todo!();
+    pub fn cycle(&mut self) {
+        let opcode = self.fetch_next();
+        self.execute(opcode);
+    }
+
+    fn fetch_next(&mut self) -> u8 {
+        let byte = self.memory[self.program_counter as usize];
+        self.program_counter += 1;
+        byte
+    }
+
+    fn execute(&mut self, opcode: u8) {
+        // Super big and ugly match statement because I'm not sure of a better way and I don't have
+        match opcode {
+            0x00 => (), // NOP
+            0x01 => { // LXI rpBC,d16
+                self.c_reg = self.fetch_next();
+                self.b_reg = self.fetch_next();
+            },
+            _ => panic!("Attempted to execute undefined instruction {:#03x}", opcode)
+        }
     }
 }
